8f9e07dbe0e43f8a76c41f8318db2549087e3cbf
Aakrati Gupta <147893415+aakrati14@users.noreply.github.com>
2024-10-13 16:33:21 +0530

    Time: 0 ms (100%), Space: 8.4 MB (41.86%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        
-        if(n < 0) {
-            x = 1 / x;
-        } 
-        
-        long num = labs(n);
-        
-        double pow = 1;
-        
-        while(num){ // equivalent to while(num != 0)
-            if(num & 1) { // equivalent to if((num & 1) != 0)
-                pow *= x;
-            }
-            
-            x *= x;
-            num >>= 1;
+        long binf=n;
+        if(n<0){
+            x=1/x;
+            binf=-binf;
         }
-        
-        return pow;
+        double ans=1;
+        while(binf>0){
+            if(binf%2==1){
+            ans*=x;
+        }
+        x*=x;
+        binf/=2;
     }
+        return ans;
+}
+    
 };
